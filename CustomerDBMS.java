1b40a96ac5113b52bb3116d2f167045cd1a96ed7
Neil Patel <nmp61@pitt.edu>
2018-06-02 11:46:25 -0400

    initial commit

---
+++ CustomerDBMS.java
@@ -0,0 +1,17 @@
+/*
+*  Author: Neil Patel
+*  GitHub: neilpatel
+*  Project: Customer DBMS
+*/
+
+// import statements
+import java.io.FileNotFoundException;
+
+// start of project
+@SuppressWarnings({ "serial" })
+public class CustomerDBMS extends MouseEvents{
+	public static void main(Strings[] args)  throws FileNotFoundException {
+		CustomerDBMS frame = new CustomerDBMS();
+		frame.setVisible(true);
+	}
+}
