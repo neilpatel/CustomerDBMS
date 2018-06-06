735d33ff8a92944468ccefdda2b5d4c924ed5db0
Neil Patel <nmp61@pitt.edu>
2018-06-06 08:54:41 -0400

    initial commit

---
+++ FileSplitter.java
@@ -0,0 +1,29 @@
+/*
+*  Author: Neil Patel
+*  GitHub: neilpatel
+*  Project: Customer DBMS
+*/
+
+// import statements
+import java.io.*;
+import java.util.Scanner;
+
+public class FileSplitter {
+	private Scanner input;
+	private File filePath;
+	
+	//=================================================================
+	//==================== overloaded constructor =====================
+	
+	public FileParser(File file) throws FileNotFoundException {
+		input = new Scanner(file);
+		filePath = file;
+	}
+
+
+
+
+
+
+
+}
