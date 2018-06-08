cd77da2f7b977ce9cbc113005fc4941091ea7369
Neil Patel <nmp61@pitt.edu>
2018-06-08 19:57:13 -0400

    Add Comparator functionality

--- Address.java
+++ Address.java
@@ -9,6 +9,19 @@
 import java.util.Comparator;
 
 public class Address implements Comparator<Customer> {
-
+	SortOrder sortOrder;
+	
+	public AddressComparator(SortOrder sortOrder) {
+		this.sortOrder = sortOrder;
+	}
+	
+	@Override
+	public int compare(Customer c1, Customer c2) {
+		String address1 = c1.getAddress();
+		String address2 = c2.getAddress();
+		
+		if(sortOrder == SortOrder.ascending)return address1.compareTo(address2);
+		else return address2.compareTo(address1);
+	}
 
 }
