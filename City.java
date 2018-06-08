9d885f61e9305f54281862da64687023cb8de9a3
Neil Patel <nmp61@pitt.edu>
2018-06-08 19:59:15 -0400

    Initial commit

---
+++ City.java
@@ -0,0 +1,15 @@
+/*
+*  Author: Neil Patel
+*  GitHub: neilpatel
+*  Project: Customer DBMS
+*/
+
+// import statements
+
+import java.util.Comparator;
+
+public class City implements Comparator<Customer> {
+
+
+
+}
