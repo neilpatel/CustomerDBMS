225399b640e7cc7b287bed8a89dfb8771e000339
Neil Patel <nmp61@pitt.edu>
2018-06-08 19:59:59 -0400

    Add City Comparator functionality

--- City.java
+++ City.java
@@ -9,7 +9,20 @@
 import java.util.Comparator;
 
 public class City implements Comparator<Customer> {
+	SortOrder sortOrder;
+	
+	public CityComparator(SortOrder sortOrder) {
+		this.sortOrder = sortOrder;
+	}
 
+	@Override
+	public int compare(Customer c1, Customer c2) {
+		String city1 = c1.getCity();
+		String city2 = c2.getCity();
+		
+		if(sortOrder == SortOrder.ascending)return city1.compareTo(city2);
+		else return city2.compareTo(city1);
+	}
 
 
 }
