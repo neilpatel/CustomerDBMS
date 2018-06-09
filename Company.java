b79d0a3b6438bbf64bdfa204d619462386e9ae52
Neil Patel <nmp61@pitt.edu>
2018-06-08 20:01:48 -0400

    Add Company Comparator Functionality

---
+++ Company.java
@@ -0,0 +1,25 @@
+/*
+*  Author: Neil Patel
+*  GitHub: neilpatel
+*  Project: Customer DBMS
+*/
+
+// import statements
+
+public class Company implements Comparator<Customer> {
+	SortOrder sortOrder;
+	
+	public CompanyComparator(SortOrder sortOrder) {
+		this.sortOrder = sortOrder;
+	}
+
+	@Override
+	public int compare(Customer c1, Customer c2) {
+		String company1 = c1.getCompany();
+		String company2 = c2.getCompany();
+		
+		if(sortOrder == SortOrder.ascending)return company1.compareTo(company2);
+		else return company2.compareTo(company1);
+	}
+
+}
