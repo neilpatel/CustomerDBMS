c977c07731f27235236f6fcbc034f22085dd7100
Neil Patel <nmp61@pitt.edu>
2018-06-09 10:15:24 -0400

    Add First Name Comparator Functionality

---
+++ FirstName.java
@@ -0,0 +1,26 @@
+/*
+*  Author: Neil Patel
+*  GitHub: neilpatel
+*  Project: Customer DBMS
+*/
+
+// import statements
+import java.util.Comparator;
+import java.util.Comparator;
+
+public class FirstNameComparator implements Comparator<Customer> {
+	SortOrder sortOrder;
+	
+	public FirstNameComparator(SortOrder sortOrder) {
+		this.sortOrder = sortOrder;
+	}
+
+	@Override
+	public int compare(Customer c1, Customer c2) {
+		String firstName1 = c1.getFirstName();
+		String firstName2 = c2.getFirstName();
+		
+		if(sortOrder == SortOrder.ascending)return firstName1.compareTo(firstName2);
+		else return firstName2.compareTo(firstName1);
+	}
+}
