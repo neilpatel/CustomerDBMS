31531f55bea84bc76518b906aacc130ca60ae07c
Neil Patel <nmp61@pitt.edu>
2018-06-11 08:33:27 -0400

    initial commit

---
+++ LastName.java
@@ -0,0 +1,25 @@
+/*
+*  Author: Neil Patel
+*  GitHub: neilpatel
+*  Project: Customer DBMS
+*/
+
+// import statements
+import java.util.Comparator;
+
+public class LastName implements Comparator<Customer> {
+	SortOrder sortOrder;
+	
+	public LastName(SortOrder sortOrder) {
+		this.sortOrder = sortOrder;
+	}
+
+	@Override
+	public int compare(Customer c1, Customer c2) {
+		String lastName1 = c1.getLastName();
+		String lastName2 = c2.getLastName();
+		
+		if(sortOrder == SortOrder.ascending)return lastName1.compareTo(lastName2);
+		else return lastName2.compareTo(lastName1);
+	}
+}
