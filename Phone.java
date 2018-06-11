fb9132e727c0adafb867536c0e869efcdf4d3bfa
Neil Patel <nmp61@pitt.edu>
2018-06-11 08:35:53 -0400

    Initial commit

---
+++ Phone.java
@@ -0,0 +1,25 @@
+/*
+*  Author: Neil Patel
+*  GitHub: neilpatel
+*  Project: Customer DBMS
+*/
+
+// import statements
+import java.util.Comparator;
+
+public class Phone implements Comparator<Customer> {
+	SortOrder sortOrder;
+	
+	public Phone(SortOrder sortOrder) {
+		this.sortOrder = sortOrder;
+	}
+
+	@Override
+	public int compare(Customer c1, Customer c2) {
+		String phone1 = c1.getPhone();
+		String phone2 = c2.getPhone();
+		
+		if(sortOrder == SortOrder.ascending)return phone1.compareTo(phone2);
+		else return phone2.compareTo(phone1);
+	}
+}
