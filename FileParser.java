bfff43dca5beed75bdc5057c1ccfd2055827dee3
Neil Patel <nmp61@pitt.edu>
2018-06-13 11:20:23 -0400

    Fixed small issue

--- FileParser.java
+++ FileParser.java
@@ -23,12 +23,11 @@ public class FileParser {
 		
 		Customer[] customers = new Customer[arrayCapacity(filePath)];
 		input.nextLine(); // gets rid of header
-		
-		for(int i=0; i<customers.length; i++){
+		for(int i = 0; i < customers.length; i++){
 			line = input.nextLine();					// tokenizes each line of input
 			line = line.substring(1, line.length()-1);	// removes the first and last characters (")
 			tokens = line.split("\",\"");				// splits each line into an array of strings to construct a customer
-			customers[i] = new Customer(tokens[0],tokens[1],tokens[2],tokens[3], tokens[4], tokens[5], tokens[6], tokens[7], tokens[8], tokens[9], tokens[10], tokens[11]);
+			customers[i] = new Customer(tokens[0], tokens[1], tokens[2], tokens[3], tokens[4], tokens[5], tokens[6], tokens[7], tokens[8], tokens[9], tokens[10], tokens[11]);
 		}
 		
 		input.close();
